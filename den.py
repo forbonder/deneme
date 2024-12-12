54290831bbf5f8d02101bc498f99616bcccce773
ilim2bilim <73699556+forbonder@users.noreply.github.com>
2024-12-12 21:36:37 +0300

    Update den.py

--- den.py
+++ den.py
@@ -6,8 +6,3 @@ from numba import prange, jit
 import time as tm
 from scipy.interpolate import griddata
 
-TT = tm.time()         
-def lrange(r1, inc, r2):
-    """Matlabdaki " : " operatoru icin, scicomptan alinti"""
-    n = ((r2-r1)+2*np.spacing(r2-r1))//inc
-    return np.linspace(r1,r1+inc*n,n+1)
